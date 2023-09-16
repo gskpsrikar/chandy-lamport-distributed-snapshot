70d13510bb738ebc135cf63d8a334cddf1520b95
gskpsrikar <gskps2160@gmail.com>
2023-09-16 12:56:26 -0500

    Hello World Async!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int waitTime = 3;
+
+        for (int i=0; i < waitTime; i++) {
+
+            String message = String.format("Hello World! The time is %d seconds", i+1);
+            System.out.println(message);
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+        
     }
 }
