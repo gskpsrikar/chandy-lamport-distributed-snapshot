fcb370a89a5facef273f0b6f4c34afe8c055ab04
gskpsrikar <gskps2160@gmail.com>
2023-10-09 00:31:11 -0500

    DEBUG: ChandyLamport

--- ChandyLamport.java
+++ ChandyLamport.java
@@ -11,24 +11,26 @@ public class ChandyLamport {
 
     public void dfs() throws Exception {
         try {
-            System.out.println("Initiating Snapshot DFS function in 3 seconds");
+            System.out.println("[THREAD] Initiating Snapshot DFS function in 3 seconds");
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        for (Map.Entry<Integer, SctpChannel> entry : m.idToChannelMap.entrySet()) {
-            
-            Integer nodeId = entry.getKey();
-            SctpChannel channel = entry.getValue();
-            
-            System.out.println("Key: " + nodeId + ", Channel: " + channel);
+        System.out.println("[DEBUG] IdToChannelMap = " + m.idToChannelMap);
 
-            Message msg = new Message(m.node.nodeId);
+        // for (Map.Entry<Integer, SctpChannel> entry : m.idToChannelMap.entrySet()) {
+            
+        //     Integer nodeId = entry.getKey();
+        //     SctpChannel channel = entry.getValue();
+            
+        //     System.out.println("Key: " + nodeId + ", Channel: " + channel);
 
-            synchronized(m) {
-                Client.send_message(msg, channel, m);
-            }
+        //     Message msg = new Message(m.node.nodeId);
+
+        //     synchronized(m) {
+        //         Client.send_message(msg, channel, m);
+        //     }
         }
     }
 }
